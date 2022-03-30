7f06d230c913607a2d4e3660618cf8d7da4c125f
taeyun1 <ew20@naver.com>
2022-03-30 23:34:19 +0900

    first commit

---
+++ server/Model/Post.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: String,
+    content: String,
+  },
+  { collation: "posts" }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
+// module.exports = mongoose.model("Post", postSchema);
